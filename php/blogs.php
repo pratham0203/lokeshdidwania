bf59b00966d67ddcca5cbf4efde4a75bb9867b02
Aharnish KS <78910585+Adensty7@users.noreply.github.com>
2021-07-14 13:57:03 +0530

    Update php files

--- php/blogs.php
+++ php/blogs.php
@@ -1,13 +1,26 @@
 <?php
 include 'connection.php';
 if(isset($_POST['submit'])){
-$blog_id = $_POST['blid'];
 $blog_title = $_POST['bltitle'];
 $blog_content = $_POST['content'];
 $blog_img = $_POST['image'];
 $blog_author = $_POST['author'];
 $date = date("Y-m-d");
 $isVisible = $_POST['visible'];
+
+$select_query = "SELECT count(Blog_ID) from blogs"; 
+
+$query = mysqli_query($conn, $select_query);
+
+$check = mysqli_fetch_array($query);
+
+if($check==NULL){
+    $blog_id = 1;
+}
+else {
+    $blog_id = $check['count(Blog_ID)'] + 1;   
+}
+
 
 $sql_query = "INSERT INTO blogs(Blog_ID, Blog_Title, Blog_Content, Blog_Image, Blog_Author, Date, Visible)
 VALUES ('$blog_id', '$blog_title', '$blog_content', '$blog_img', '$blog_author', '$date','$isVisible')";
--- php/books.php
+++ php/books.php
@@ -1,12 +1,24 @@
 <?php
 include 'connection.php';
 if(isset($_POST['submit'])){
-$book_id = $_POST['bkid'];
 $book_name = $_POST['bkname'];
 $book_cover = $_POST['cover'];
 $book_desc = $_POST['desc'];
 $price = $_POST['price'];
 $isVisible = $_POST['visible'];
+
+$select_query = "SELECT count(Book_ID) from books"; 
+
+$query = mysqli_query($conn, $select_query);
+
+$check = mysqli_fetch_array($query);
+
+if($check==NULL){
+    $book_id = 1;
+}
+else {
+    $book_id = $check['count(Book_ID)'] + 1;   
+}
 
 $sql_query = "INSERT INTO books(Book_ID, Book_Name, Book_Cover, Book_Description, Price, Visible)
 VALUES ('$book_id', '$book_name', '$book_cover', '$book_desc', '$price', '$isVisible')";
