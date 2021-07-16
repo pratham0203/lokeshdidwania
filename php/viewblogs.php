492e14dac87bfadab9a3dfc25dd7124f3f68afba
Aharnish KS <78910585+Adensty7@users.noreply.github.com>
2021-07-17 02:27:09 +0530

    Updated viewblogs.php
    Now, admin can view all the comments to each blog.

--- php/viewblogs.php
+++ php/viewblogs.php
@@ -19,7 +19,25 @@ while ($res = mysqli_fetch_array($query)) {
 <!-- HTML Code -->
 
 <?php
+    $select_query2 = " select * from blog_comments where Blog_ID='$id'";
 
+    $query2 = mysqli_query($conn, $select_query2);
+    while ($res2 = mysqli_fetch_array($query2)) {
+        $cid = $res['Comment_ID'];
+        $bid = $res['Blog_ID'];
+        $name = $res['Name'];
+        $email = $res['Email'];
+        $comment = $res['Comment'];
+        $date = $res['Date'];
+        $time = $res['Time'];
+ ?>
+
+
+<!-- HTML Code -->
+
+
+<?php
+}
 }
 
 ?>
