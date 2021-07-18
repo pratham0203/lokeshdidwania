71ab63bcbfd361a82ca4e4405319fc17a1169bf1
pratham0203 <prathamtripathi011@gmail.com>
2021-07-18 17:38:40 +0530

    Title changes

--- admin/pages/examples/blogs-edit.php
+++ admin/pages/examples/blogs-edit.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Blog Edit</title>
+    <title>Admin Panel | Blog Edit</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
