a1197215d69e21e6a8287e2c922dcb20556816fd
Aharnish KS <78910585+Adensty7@users.noreply.github.com>
2021-07-19 20:25:27 +0530

    Added examples pages

--- admin/pages/examples/books-edit.php
+++ admin/pages/examples/books-edit.php
@@ -59,65 +59,77 @@
                 </li>
 
                 <!-- Messages Dropdown Menu -->
+                <?php 
+                    include 'connection.php';
+
+                    $select_query = "select count(message_id) from contact_message where notify = 'Yes'";
+
+                    $query = mysqli_query($conn, $select_query);
+
+                    while ($res = mysqli_fetch_array($query)) {
+                        $count = $res['count(message_id)'];
+                ?>
+                <!-- Messages Dropdown Menu -->
                 <li class="nav-item dropdown">
                     <a class="nav-link" data-toggle="dropdown" href="#">
                         <i class="far fa-comments"></i>
-                        <span class="badge badge-danger navbar-badge">3</span>
+                        <span class="badge badge-danger navbar-badge"><?php echo $count; ?></span>
                     </a>
+
+                    <?php
+                        }
+                    
+                    ?>
                     <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                        <!-- Message Start -->
+                        <?php
+
+                                $select_query = " select * from contact_message
+                                where notify = 'Yes'
+                                order by message_id desc";
+
+                                $query = mysqli_query($conn, $select_query);
+
+                                while ($res = mysqli_fetch_array($query)) {
+                                    $id = $res['message_id'];
+                                    $name = $res['name'];
+                                    $email = $res['email'];
+                                    $subject = $res['subject'];
+                                    $message = $res['message'];
+                                    $notify = $res['notify'];
+                            ?>
                         <a href="#" class="dropdown-item">
-                            <!-- Message Start -->
                             <div class="media">
-                                <img src="../../dist/img/user1-128x128.jpg" alt="User Avatar"
-                                    class="img-size-50 mr-3 img-circle">
+                                <!-- <img src="dist/img/user1-128x128.jpg" alt="User Avatar"
+                                    class="img-size-50 mr-3 img-circle"> -->
                                 <div class="media-body">
                                     <h3 class="dropdown-item-title">
-                                        Brad Diesel
+                                        <?php echo $name; ?>
                                         <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
                                     </h3>
-                                    <p class="text-sm">Call me whenever you can...</p>
-                                    <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+                                    <p class="text-sm"><?php echo substr($message, 0, 100); ?></p>
+                                    <!-- <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p> -->
                                 </div>
                             </div>
                             <!-- Message End -->
                         </a>
                         <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item">
-                            <!-- Message Start -->
-                            <div class="media">
-                                <img src="../../dist/img/user8-128x128.jpg" alt="User Avatar"
-                                    class="img-size-50 img-circle mr-3">
-                                <div class="media-body">
-                                    <h3 class="dropdown-item-title">
-                                        John Pierce
-                                        <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
-                                    </h3>
-                                    <p class="text-sm">I got your message bro</p>
-                                    <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                                </div>
-                            </div>
-                            <!-- Message End -->
-                        </a>
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item">
-                            <!-- Message Start -->
-                            <div class="media">
-                                <img src="../../dist/img/user3-128x128.jpg" alt="User Avatar"
-                                    class="img-size-50 img-circle mr-3">
-                                <div class="media-body">
-                                    <h3 class="dropdown-item-title">
-                                        Nora Silvester
-                                        <span class="float-right text-sm text-warning"><i
-                                                class="fas fa-star"></i></span>
-                                    </h3>
-                                    <p class="text-sm">The subject goes here</p>
-                                    <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                                </div>
-                            </div>
-                            <!-- Message End -->
-                        </a>
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
+                        <?php
+                        }
+
+                        /* function updateNotifications($con) {
+                            $update_query = "update contact_message
+                            set notify = 'No'
+                            where notify = 'Yes'";
+                        
+                            $update = mysqli_query($con,$update_query);
+                        } */
+                        
+                        ?>
+                        <a href="#" onclick="<?php // updateNotifications($conn); ?>"
+                            class="dropdown-item dropdown-footer">See
+                            All
+                            Messages</a>
                     </div>
                 </li>
                 <!-- Notifications Dropdown Menu -->
@@ -202,7 +214,7 @@
                         <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
                         <li class="nav-item menu-open">
-                            <a href="/admin/index.html" class="nav-link active">
+                            <a href="/admin/index.php" class="nav-link active">
                                 <i class="nav-icon fas fa-tachometer-alt"></i>
                                 <p>
                                     Dashboard
