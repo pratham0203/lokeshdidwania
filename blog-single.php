76babc16cca9795b9170b6ffd8a3ac08451927e3
Aharnish KS <78910585+Adensty7@users.noreply.github.com>
2021-07-22 17:07:20 +0530

    Updated footer

--- blog-single.php
+++ blog-single.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>Folio Bootstrap Template - Blog Single</title>
+    <title> Blog </title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
@@ -36,19 +36,19 @@
 
             <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a> -->
             <!-- Uncomment below if you prefer to use an text logo -->
-            <h1 class="logo"><a href="index.html">Lokesh Didwania</a></h1>
+            <h1 class="logo"><a href="/">Lokesh Didwania</a></h1>
 
             <nav id="navbar" class="navbar">
                 <ul>
-                    <li><a class="nav-link scrollto active" href="index.html#hero">Home</a></li>
-                    <li><a class="nav-link scrollto" href="index.html#about">About Me </a></li>
-                    <li><a class="nav-link  scrollto" href="books.html">My Books</a></li>
-                    <li><a class="nav-link  scrollto" href="podcast.html">My Podcasts</a></li>
-                    <li><a class="nav-link  scrollto" href="blogs.html"> My Blogs</a></li>
+                    <li><a class="nav-link scrollto" href="/#hero">Home</a></li>
+                    <li><a class="nav-link scrollto" href="/#about">About Me </a></li>
+                    <li><a class="nav-link  scrollto" href="books.php">My Books</a></li>
+                    <li><a class="nav-link  scrollto" href="podcast">My Podcasts</a></li>
+                    <li><a class="nav-link  scrollto" href="blogs.php"> My Blogs</a></li>
                     <li class="dropdown"><a href="#"><span>Other Options</span> <i class="bi bi-chevron-down"></i></a>
                         <ul>
-                            <li><a href="contact.html">Contact Me</a></li>
-                            <li><a href="tnc.html">Terms and Conditions</a></li>
+                            <li><a href="contact">Contact Me</a></li>
+                            <li><a href="tnc">Terms and Conditions</a></li>
                         </ul>
                     </li>
                 </ul>
@@ -234,7 +234,7 @@
                     ?>
 
         <script>
-        window.location.replace("blogs.php");
+        window.location.href = "";
         </script>
         <?php
                     }
@@ -242,7 +242,7 @@
                     ?>
         <script>
         alert("Unable to post comment");
-        window.location.replace("blogs.php");
+        window.location.href = "";
         </script>
         <?php
                     }
@@ -264,18 +264,11 @@
 
             </div>
 
-            <p>&copy; Copyrights Folio. All rights reserved.</p>
-
+            <p>&copy; Lokesh Didwania. All rights reserved.</p>
             <div class="credits">
-                <!--
-        All the links in the footer should remain intact.
-        You can delete the links only if you purchased the pro version.
-        Licensing information: https://bootstrapmade.com/license/
-        Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Folio
-      -->
-                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+                <p>Designed By <a class="footerlink" href="https://www.webdeveloperrajatagrawal.com/">R A Creators</a>
+                </p>
             </div>
-
         </div>
     </div><!-- End Footer -->
 
